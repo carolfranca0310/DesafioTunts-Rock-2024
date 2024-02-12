56822464566aaf84a6b532b0d3c2e1533746924f
CarolAraujo <carolfranca0310@gmail.com>
2024-02-12 15:59:43 -0300

    feat: model que representa a planilha de alunos

---
+++ desafioTuntsRock2024/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace desafioTuntsRock2024.Models
+{
+    public class Student
+    {
+        public int Registration { get; set; }
+        public string Name { get; set; }
+        public int Absence { get; set; }
+        public float P1 { get; set; }
+        public float P2 { get; set; }
+        public float P3 { get; set; }
+        public string Situation { get; set; }
+        public int FinalPassing { get; set; }
+    }
+}
