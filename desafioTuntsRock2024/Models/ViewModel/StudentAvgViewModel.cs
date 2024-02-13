a7f985de84a047035de172be33561f94b9984d8c
CarolAraujo <carolfranca0310@gmail.com>
2024-02-13 15:50:23 -0300

    feat: criada as view models para auxiliar os transportes de dados

---
+++ desafioTuntsRock2024/Models/ViewModel/StudentAvgViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace desafioTuntsRock2024.Models.ViewModel
+{
+    public class StudentAvgViewModel
+    {
+        public int Registration { get; set; }
+        public float FinalGrade { get; set; }
+    }
+}
---
+++ desafioTuntsRock2024/Models/ViewModel/StudentGradeRecordViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace desafioTuntsRock2024.Models.ViewModel
+{
+    public class StudentGradeRecordViewModel
+    {
+        public int Registration { get; set; }
+        public float P1 { get; set; }
+        public float P2 { get; set; }
+        public float P3 { get; set; }
+        public float FinalGrade { get; set; }
+    }
+}
