23fec67982b98252f9563d5b0eacac2d5b702841
CarolAraujo <carolfranca0310@gmail.com>
2024-02-13 15:48:49 -0300

    feat: adicionado enum com a lista de possibilidades da propriedade Situation

---
+++ desafioTuntsRock2024/Models/Enum/SituationEnum.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace desafioTuntsRock2024.Models
+{
+    public enum SituationEnum
+    {
+        [Description("Reprovado Por Falta")]
+        AbsenteeFailure,
+
+        [Description("Aprovado")]
+        Passed,
+
+        [Description("Reprovado Por Nota")]
+        GradeFailure,
+
+        [Description("Exame Final")]
+        FinalExam
+
+    }
+}
