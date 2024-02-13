338b541118b63b22f4e9411b3cc7bb0bd5c842fd
CarolAraujo <carolfranca0310@gmail.com>
2024-02-13 15:51:06 -0300

    refactor: removida as funções desnecessárias

--- desafioTuntsRock2024/Controllers/HomeController.cs
+++ desafioTuntsRock2024/Controllers/HomeController.cs
@@ -1,25 +1,18 @@
 ﻿using desafioTuntsRock2024.Helper;
-using desafioTuntsRock2024.Models;
-using Google.Apis.Sheets.v4.Data;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace desafioTuntsRock2024.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-
         }
 
         public IActionResult Index()
         {
-
             return View();
         }
 
@@ -28,10 +21,5 @@ namespace desafioTuntsRock2024.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
