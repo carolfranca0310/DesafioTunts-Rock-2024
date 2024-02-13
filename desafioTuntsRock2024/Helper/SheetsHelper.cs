28710f718a3259228da391c8a84014b454167d5a
CarolAraujo <carolfranca0310@gmail.com>
2024-02-13 15:46:11 -0300

    feat: implementado funções necessárias para leitura e manipulação da planilha

--- desafioTuntsRock2024/Helper/SheetsHelper.cs
+++ desafioTuntsRock2024/Helper/SheetsHelper.cs
@@ -1,4 +1,7 @@
-﻿using Google.Apis.Auth.OAuth2;
+﻿using desafioTuntsRock2024.Extensions;
+using desafioTuntsRock2024.Models;
+using desafioTuntsRock2024.Models.ViewModel;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
@@ -9,8 +12,8 @@ namespace desafioTuntsRock2024.Helper
     public class SheetsHelper
     {
         private static readonly string[] _scope = { SheetsService.Scope.Spreadsheets };
-        private static readonly string _pageName = "Página1";
-        private static readonly string _worksheetId = "1zAf-k_maGFfR78mR269QbSvfHi8DxnFImwHH0FIPGTk";
+        private static readonly string _pageName = "engenharia_de_software";
+        private static readonly string _worksheetId = "1eiRlbDDnbfkN4XjpXdPayqiJpcsQc-tVzcl3jW5lyoo";
         private SheetsService _sheetsService;
 
         public SheetsHelper()
@@ -26,11 +29,9 @@ namespace desafioTuntsRock2024.Helper
 
         public IList<IList<object>> SpreadSheet()
         {
-            var range = $"{_pageName}!A:F";
+            var range = $"{_pageName}!A:H";
             SpreadsheetsResource.ValuesResource.GetRequest request = _sheetsService.Spreadsheets.Values.Get(_worksheetId, range);
-            // Ecexuting Read Operation...
             var response = request.Execute();
-            // Getting all records from Column A to E...
             IList<IList<object>> values = response.Values;
 
             if (values != null && values.Count > 0)
@@ -66,62 +67,145 @@ namespace desafioTuntsRock2024.Helper
             updateRequest.Execute();
         }
 
-        public void CalcularNotas()
+        public List<Student> CalculateGrades()
         {
             var values = SpreadSheet();
-            int qtdAulas = 0;
+            int numClasses = 0;
+            List<Student> listStudentsRemediation = new();
 
             if (values != null && values.Count > 0)
             {
                 if (values[1].Count == 1 && values[1].First().ToString().Contains("Total de aulas no semestre"))
-                    qtdAulas = Convert.ToInt32(values[1].First().ToString().Split(":")[1]);
+                    numClasses = Convert.ToInt32(values[1].First().ToString().Split(":")[1]);
 
                 for (int i = 3; i < values.Count; i++)
                 {
                     if (values[i].Count >= 6)
                     {
+
                         var row = values[i];
-                        int qtdFaltas = Convert.ToInt32(row[2]);
+                        int numAbsences = Convert.ToInt32(row[2]);
                         float p1 = Convert.ToInt32(row[3]);
                         float p2 = Convert.ToInt64(row[4]);
                         float p3 = Convert.ToInt64(row[5]);
 
-                        if (qtdFaltas > (qtdAulas * 0.25))
+                        var student = new Student
                         {
-                            UpdateCell($"G{i + 4}", "Reprovado Por Falta");
-                            UpdateCell($"H{i + 4}", "0");
+                            Registration = Convert.ToInt32(row[0]),
+                            Name = row[1].ToString(),
+                            Absence = Convert.ToInt32(row[2]),
+                            P1 = Convert.ToInt64(row[3]),
+                            P2 = Convert.ToInt64(row[4]),
+                            P3 = Convert.ToInt64(row[5]),
+                        };
+
+                        if (numAbsences > (numClasses * 0.25))
+                        {
+                            UpdateCell($"G{i + 1}", SituationEnum.AbsenteeFailure.GetDescription());
+                            UpdateCell($"H{i + 1}", "0");
+
+                            student.Situation = SituationEnum.AbsenteeFailure;
                         }
                         else
                         {
-                            float media = ((p1 + p2 + p3) / 3) / 10;
-                            if (media < 5)
+                            float avg = ((p1 + p2 + p3) / 3) / 10;
+                            if (avg < 5)
                             {
-                                UpdateCell($"G{i + 1}", "Reprovado Por Nota");
+                                UpdateCell($"G{i + 1}", SituationEnum.GradeFailure.GetDescription());
                                 UpdateCell($"H{i + 1}", "0");
+
+                                student.Situation = SituationEnum.GradeFailure;
                             }
 
-                            else if (media >= 7)
+                            else if (avg >= 7)
                             {
-                                UpdateCell($"G{i + 1}", "Aprovado");
+                                UpdateCell($"G{i + 1}", SituationEnum.Passed.GetDescription());
                                 UpdateCell($"H{i + 1}", "0");
+
+                                student.Situation = SituationEnum.Passed;
                             }
                             else
                             {
-                                Console.Write("Insira a nota da Avaliação Final: ");
-                                string nafstr = Console.ReadLine().Replace(".", ",");
-
-                                if (float.TryParse(nafstr, out float naf))
-                                {
-                                    UpdateCell($"G{i + 1}", "Exame Final");
-                                    UpdateCell($"H{i + 1}", $"{((media + naf) / 2):n1}");
-
-                                }
+                                student.Situation = SituationEnum.FinalExam;
+                                UpdateCell($"G{i + 1}", SituationEnum.FinalExam.GetDescription());
                             }
                         }
+
+                        listStudentsRemediation.Add(student);
                     }
                 }
             }
+
+            return listStudentsRemediation;
+        }
+
+        public IEnumerable<StudentAvgViewModel> CalculateAverage(List<StudentGradeRecordViewModel> studentsGradeRecord)
+        {
+            var listSudentAvg = new List<StudentAvgViewModel>();
+
+            foreach (var student in studentsGradeRecord)
+            {
+                float avg = ((student.P1 + student.P2 + student.P3) / 3) / 10;
+                
+                UpdateCell($"H{student.Registration + 3}", $"{((avg + student.FinalGrade) / 2):n1}");
+
+                listSudentAvg.Add(new StudentAvgViewModel
+                {
+                    Registration = student.Registration,
+                    FinalGrade = student.FinalGrade
+                });
+            }
+
+            return listSudentAvg;
+        }
+
+        public List<Student> ConvertSpreadsheet(IList<IList<object>> values)
+        {
+            List<Student> listStudents = new List<Student>();
+
+            for (int i = 3; i < values.Count; i++)
+            {
+                if (values[i].Count >= 6)
+                {
+
+                    var row = values[i];
+
+
+                    var student = new Student
+                    {
+                        Registration = Convert.ToInt32(row[0]),
+                        Name = row[1].ToString(),
+                        Absence = Convert.ToInt32(row[2]),
+                        P1 = Convert.ToInt64(row[3]),
+                        P2 = Convert.ToInt64(row[4]),
+                        P3 = Convert.ToInt64(row[5]),
+                    };
+
+                    if (row.ElementAtOrDefault(6).ToString() != 0.ToString())
+                    {
+                        if (row[6].ToString() == SituationEnum.FinalExam.GetDescription())
+                            student.Situation = SituationEnum.FinalExam;
+
+                        else if (row[6].ToString() == SituationEnum.AbsenteeFailure.GetDescription())
+                            student.Situation = SituationEnum.AbsenteeFailure;
+
+                        else if (row[6].ToString() == SituationEnum.Passed.GetDescription())
+                            student.Situation = SituationEnum.Passed;
+
+                        else if (row[6].ToString() == SituationEnum.GradeFailure.GetDescription())
+                            student.Situation = SituationEnum.GradeFailure;
+                    }
+
+                    if (row.Count >= 7) 
+                        student.FinalPassing = float.Parse((row[7].ToString()));
+
+                    listStudents.Add(student);
+
+                }
+            }
+            return listStudents;
         }
     }
 }
+
 
