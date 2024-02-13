be03a30c9a1e90d5e6b37b0cf841886edee13c84
CarolAraujo <carolfranca0310@gmail.com>
2024-02-13 15:55:31 -0300

    chore: deletado arquivos que não iam ser utilizados

--- desafioTuntsRock2024/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace desafioTuntsRock2024.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
